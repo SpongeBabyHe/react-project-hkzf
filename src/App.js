a20285c9290cee8dfe83d9cb12744ef6479c3100
SpongeBabyHe <spongebabyhe@outlook.com>
2025-10-08 16:42:13 +0800

    feat: integrate antd-mobile UI and add react-router setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-
+import React from "react";
+//导入组件
+import { Button } from "antd-mobile";
+//导入路由组件
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">项目根组件</div>
+    <div className="App">项目根组件
+      <Button>登陆</Button>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+//导入antd-mobile样式
+import 'antd-mobile/es/global';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
