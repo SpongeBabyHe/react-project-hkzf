6963b3f37e46d163660a1eb3efd3cd537cf347ea
SpongeBabyHe <spongebabyhe@outlook.com>
2025-10-22 11:18:05 +0800

    feat(ui): add navigation grid with icons to Index page
    - Add 4x1 navigation grid using antd-mobile Grid component
    - Add navigation icons: 整租, 合租, 地图找房, 去出租
    - Import navigation icon images from assets directory
    - Create index.css for centralized style management
    - Optimize CSS selectors using parent className approach
    - Remove inline styles for better maintainability

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,7 +1,11 @@
 import React from "react";
-import { Swiper } from "antd-mobile";
+import { Swiper, Grid } from "antd-mobile";
+import img1 from "../../assets/images/nav1.png";
+import img2 from "../../assets/images/nav2.png";
+import img3 from "../../assets/images/nav3.png";
+import img4 from "../../assets/images/nav1.png";
+import "./index.css";
 
-// 替换为你的图片路径
 const images = [
   '/images/1.jpg',
   '/images/2.jpg',
@@ -29,6 +33,32 @@ export default class Index extends React.Component {
             </Swiper.Item>
           ))}
         </Swiper>
+        <Grid columns={4} gap={8} className="nav-grid-container">
+          <Grid.Item>
+            <div>
+              <img src={img1} alt="img1" />
+              <span>整租</span>
+            </div>
+          </Grid.Item>
+          <Grid.Item>
+            <div>
+              <img src={img2} alt="img2" />
+              <span>合租</span>
+            </div>
+          </Grid.Item>
+          <Grid.Item>
+            <div>
+              <img src={img3} alt="img3" />
+              <span>地图找房</span>
+            </div>
+          </Grid.Item>
+          <Grid.Item>
+            <div>
+              <img src={img4} alt="img4" />
+              <span>去出租</span>
+            </div>
+          </Grid.Item>
+        </Grid>
       </div>
     );
   }
